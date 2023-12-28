68c9529f22e50ff4ab2c93df41d3c63c70be242f
ishcenko <a_ishcenko@i.ua>
2023-12-28 13:35:16 +0200

    створення компоненту

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import ReactDOM from 'react-dom';
+import App from './App';
+// import paintings from './paintings.json';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// function Painting({ url, title, profileUrl, author, price }) {
+//   return (
+//     <div>
+//       <img src={url} alt={title} width="480" />
+//       <h2>{title}</h2>
+//       <p>
+//         Автор: <a href={profileUrl}>{author}</a>
+//       </p>
+//       <p>Ціна: {price} кредитів</p>
+//       <p>Доступність: закінчується або є в наявності</p>
+//       <button type="button">Додати у кошик</button>
+//     </div>
+//   );
+// }
+// const painting = paintings[1];
+// <Painting
+//   url={painting.url}
+//   title={painting.title}
+//   author={painting.author.tag}
+//   profileUrl={painting.author.url}
+//   price={painting.price}
+// />;
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+// import { App } from 'components/App';
+// import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
